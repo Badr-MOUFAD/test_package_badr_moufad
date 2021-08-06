1aa092238ff46589c3fb57129521db9108194fd0
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2021-08-06 22:55:44 +0100

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Test-publish-my-own-package
 
-A test repo to publish a python package to PyPI.
+A test repo to publish a python package to **PyPI**.
+
+# Requirements and general steps to follow
+
+1. The root directory should contain: 
+    - the pacakge folder. Insure to include whithin it a `__init__.py` file
+    - `setup.py` file, which is mandatory for **PyPi**)
+    - `setup.cfg`
+    - `README.md` and a `LICENSE.txt`
+
+
+2. In `setup.py`, make sure to give the **download_url** and **install_requires**. By clicking on release in the github repostity and following the steps you can get the url of the source code where the package. install_requires contain a list of the package dependencies.
+
+3. run the command `py setup.py sdist` to build a dist folder of the package.
+
+4. `pip install twine` to install a package needed to upload the your package to **PyPI**. Then run `twine upload dist/*` and follow the steps to upload it.
+
--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 from distutils.core import setup
+
+
 setup(
   name = 'test_package_badr_moufad',      
   packages = ['test_package_badr_moufad'],
-  version = '0.1',
+  version = '0.2',
   license='MIT', 
   description = 'A test repo to publish a python package to PyPI',
   author = 'Badr MOUFAD',
   author_email = 'Badr.MOUFAD@emines.um6p.ma',
   url = 'https://github.com/Badr-MOUFAD/test_package_badr_moufad',
-  download_url = 'https://github.com/Badr-MOUFAD/test_package_badr_moufad/archive/refs/tags/v0.1.tar.gz',
+  download_url = 'https://github.com/Badr-MOUFAD/test_package_badr_moufad/archive/refs/tags/v0.2.tar.gz',
   keywords = ['Example', 'test_package'],
   install_requires=[
           'numpy',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
