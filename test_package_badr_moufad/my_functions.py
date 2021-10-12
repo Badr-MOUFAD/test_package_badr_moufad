5065cde9e565beade34f2b7e389aec2422ab25de
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2021-10-12 18:53:14 +0100

    add requirement.txt

--- test_package_badr_moufad/my_functions.py
+++ test_package_badr_moufad/my_functions.py
@@ -2,10 +2,21 @@ import numpy as np
 
 
 def say_hello(username: str):
+    """A function to greet a user
+
+    Parameters
+    ----------
+    username : str
+        the username
+    """
+
     print(f"Hi there! Nice to meet you {username}")
     return
 
 
 def say_joke():
+    """A function to say a joke
+    """
+    
     print(f"Your as** is {np.random.rand()}\nAHAHAHAHA")
-    return 
+    return
