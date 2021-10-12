fdebdf85b109c72be9115c0e229a8dcff0e798f1
Badr MOUFAD <Badr.MOUFAD@emines.um6p.ma>
2021-10-12 18:56:30 +0100

    add docstrings & update license

--- test_package_badr_moufad/my_class.py
+++ test_package_badr_moufad/my_class.py
@@ -2,12 +2,28 @@ from test_package_badr_moufad.my_functions import say_hello, say_joke
 
 
 class Person:
+    """A person class to interact with a user
+    """
+
     def __init__(self, username: str) -> None:
+        """constructor of the class
+
+        Parameters
+        ----------
+        username : str
+            the name of the user
+        """
+
         self.username = username
-        
+
     def greet(self) -> None:
+        """To greet user
+        """
+
         say_hello(self.username)
 
     def joke(self) -> None:
+        """To tell a joke
+        """
+
         say_joke()
-        
